0f891355b807b09daf207c7fe62b51f6c9e8b76d
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-05-04 19:33:47 -0300

    update

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,86 +1,88 @@
-import React, { useState } from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import axios from 'axios'
-import styles from './Styles/styles.css'
-
+import './Styles/styles.css'
 function App() {
-  const [pokemon, setPokemon] = useState()
-  const [pokemonType, setPokemonType] = useState([])
-  const [pokemonData, setPokemonData] = useState([])
-  const [pokemonStat, setPokemonStats] = useState([])
-  
-  const getPokemon = async () => {
-    const toArray = []
-    try{
-
-        const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-        const res = await axios.get(url)
-        setPokemon(res.data.name)
-    
-        toArray.push(res.data)
-        setPokemonType(res.data.types[0].type.name)
-        setPokemonStats(res.data.stats
-            .map(s => s.base_stat)
-            .reduce((a, b) => a +b))
-        setPokemonData(toArray)
-        console.log(pokemon)
-        console.log(res.data)
-    } catch (e) {
-        alert("Pokemon não encontrado")
-        window.location.reload()
-    }
-}
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        getPokemon();
-    };
-    const handleChange = (e) => {
-        setPokemon(e.target.value.toLowerCase())
-    }
-
+  // States
+  const [pokemon, setPokemon] = useState(null)
+  // Refs
+  const inputRef = useRef()
+  // Callbacks
+  const handleSubmit = useCallback(event => {
+    event.preventDefault()
+    if (!inputRef.current.value)
+      return setPokemon(null)
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${inputRef.current.value}`)
+      .then(({ data }) => {
+        console.log({ data })
+        setPokemon({
+          sprite_src: data.sprites["front_default"],
+          height: data.height,
+          name: data.name,
+          types: data.types.map(record => record.type.name),
+          moves: data.moves,
+          stats_sum: data.stats
+            .map(record => ({
+              name: record.stat.name,
+              value: record.base_stat
+            }))
+            .reduce((a, b) => a + b.value, 0)
+        })
+      })
+      .catch(error => {
+        console.error(error)
+        setPokemon(null)
+      })
+  }, [])
+  // Render
   return (
     <>
-        <div className="search">
+      <div className="search">
         <form onSubmit={handleSubmit} >
-            <input type="text"
-            onChange={handleChange}
-
+          <input type="text"
+            ref={inputRef}
             placeholder="Qual Pokemon procura?"
-            />
+          />
         </form>
-            <button onClick={handleSubmit}>Buscar</button>
-        </div>
-
-        {pokemonData.map((data)=> {
-            return (
-                <div className="container">
-                    <div className="title">
-                    <img src= {data.sprites["front_default"]} alt="iamgem do pokemon"/>
-                    <h1>{data.name}</h1>
-                    </div>
-                    <div className="divTable">                        
-                        <div className="divTableBody"></div>
-                            <div className="divTableRow">
-                                <div className="divTableCell">Type</div>
-                                <div className="divTableCell">{pokemonType}</div>
-                            </div>
-                            <div className="divTableRow">
-                                <div className="divTableCell">Height</div>
-                                <div className="divTableCell">{data.height}</div>
-                            </div>
-                            <div className="divTableRow">
-                                <div className="divTableCell">Número de movimentos</div>
-                                <div className="divTableCell">{data.moves.length}</div>
-                            </div>
-                            <div className="divTableRow">
-                                <div className="divTableCell">Stats</div>
-                                <div className="divTableCell">{pokemonStat} de poder</div>
-                            </div>
-                    </div>
+        <button onClick={handleSubmit}>Buscar</button>
+      </div>
+      {pokemon
+        && (
+          <div className="container">
+            <div className="title">
+              <img src={pokemon.sprite_src} alt="iamgem do pokemon" />
+              <h1>{pokemon.name}</h1>
+            </div>
+            <div className="divTable">
+              <div className="divTableBody"></div>
+              <div className="divTableRow">
+                <div className="divTableCell">Type</div>
+                {pokemon.types.map((type, i) => (
+                  <div
+                    key={i}
+                    className="divTableCell"
+                  >{type}</div>
+                ))}
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Height</div>
+                <div className="divTableCell">{pokemon.height}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Número de movimentos</div>
+                <div className="divTableCell">{pokemon.moves.length}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Stats</div>
+                <div className="divTableCell">
+                  {pokemon.stats_sum}
                 </div>
-            )
-        })}
+              </div>
+            </div>
+          </div>
+        )}
     </>
-  );
+  )
 }
 
 export default App;
